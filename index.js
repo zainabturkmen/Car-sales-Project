088b6b32e0406dd4bb57707c702fd42d1869cece
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-01-17 20:54:44 +0430

    init

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // javascript
+
+const menu = document.querySelector('.menu')
+      filter = document.querySelector('.filter')
+      filterStyle = window.getComputedStyle(filter);
+
+
+menu.addEventListener('click', ()=> {
+
+    if (filterStyle.getPropertyValue('clip-path') === 'circle(0% at 100% 0%)') {
+        filter.style.clipPath = 'circle(100% at 50% 50%)'
+    }
+
+    else{
+        filter.style.clipPath = 'circle(0% at 100% 0%)'
+    }
+})
+
+document.addEventListener('mouseup', (event) => {
+    if (filterStyle.getPropertyValue('clip-path') === 'circle(100% at 50% 50%)') {
+        if(!filter.contains(event.target))
+            filter.style.clipPath = 'circle(0% at 100% 0%)'
+
+    }
+
+})
