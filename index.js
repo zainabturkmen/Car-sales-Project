2a1610f2a1d02f9a0207fabb0dc58a49fcba837e
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-01-18 18:55:15 +0430

    init

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-// // javascript
+// javascript
 
-// const menu = document.querySelector('.menu')
-//       filter = document.querySelector('.filter')
-//       filterStyle = window.getComputedStyle(filter)
-//       hearts = document.querySelectorAll('.far');
+const menu = document.querySelector('.menu')
+      filter = document.querySelector('.filter')
+      filterStyle = window.getComputedStyle(filter)
+      hearts = document.querySelectorAll('.far');
 
 
-// menu.addEventListener('click', ()=> {
+menu.addEventListener('click', ()=> {
 
-//     if (filterStyle.getPropertyValue('clip-path') === 'circle(0% at 100% 0%)') {
-//         filter.style.clipPath = 'circle(100% at 50% 50%)'
-//     }
+    if (filterStyle.getPropertyValue('clip-path') === 'circle(0% at 100% 0%)') {
+        filter.style.clipPath = 'circle(100% at 50% 50%)'
+    }
 
-//     else{
-//         filter.style.clipPath = 'circle(0% at 100% 0%)'
-//     }
-// })
+    else{
+        filter.style.clipPath = 'circle(0% at 100% 0%)'
+    }
+})
 
-// document.addEventListener('mouseup', (event) => {
-//     if (filterStyle.getPropertyValue('clip-path') === 'circle(100% at 50% 50%)') {
-//         if(!filter.contains(event.target))
-//             filter.style.clipPath = 'circle(0% at 100% 0%)'
+document.addEventListener('mouseup', (event) => {
+    if (filterStyle.getPropertyValue('clip-path') === 'circle(100% at 50% 50%)') {
+        if(!filter.contains(event.target))
+            filter.style.clipPath = 'circle(0% at 100% 0%)'
 
-//     }
+    }
 
-// })
+})
 
-// function changeHeart(x){
-//     x.classList.toggle('fas')
-//     x.classList.toggle('far')
-// }
+function changeHeart(x){
+    x.classList.toggle('fas')
+    x.classList.toggle('far')
+}
 
